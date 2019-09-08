baa24080864c7b2e457778cd62ee6ac403ac55f5
williamfreitas217 <williamdov.c.f@gmail.com>
2019-09-07 23:59:46 -0400

    promises

---
+++ promises/main.js
@@ -0,0 +1,24 @@
+var myPromise = function(){
+    return new Promise(function(resolve, reject){
+        var xhr =  new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/williamfreitas217');
+        xhr.send(null);
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4){
+                if (xhr.status==200){
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject('E0rro na requisicao');
+                }
+            }
+        }
+    });
+}
+
+myPromise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
